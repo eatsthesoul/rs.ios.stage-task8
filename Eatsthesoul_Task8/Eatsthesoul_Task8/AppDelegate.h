7141f09286bd6957bf22e4d8d1ca1b5505b41891
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-07-16 16:29:15 +0300

    init commit

---
+++ Eatsthesoul_Task8/Eatsthesoul_Task8/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Eatsthesoul_Task8
+//
+//  Created by Evgeniy Petlitskiy on 16.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Eatsthesoul_Task8/Eatsthesoul_Task8/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Eatsthesoul_Task8
+//
+//  Created by Evgeniy Petlitskiy on 16.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
