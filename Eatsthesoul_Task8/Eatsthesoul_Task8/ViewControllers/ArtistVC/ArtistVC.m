ff8582311a8be991eeaad49d9f3ee93d592abd8f
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-07-17 18:04:06 +0300

    feat(ArtistVC): add UI elements

--- Eatsthesoul_Task8/Eatsthesoul_Task8/ViewControllers/ArtistVC/ArtistVC.m
+++ Eatsthesoul_Task8/Eatsthesoul_Task8/ViewControllers/ArtistVC/ArtistVC.m
@@ -7,10 +7,15 @@
 
 #import "ArtistVC.h"
 #import "CanvasView.h"
+#import "RSButton.h"
 
 @interface ArtistVC ()
 
 @property (nonatomic, strong) UIView *canvasView;
+@property (nonatomic, strong) RSButton *openPaletteButton;
+@property (nonatomic, strong) RSButton *openTimerButton;
+@property (nonatomic, strong) RSButton *drawButton;
+@property (nonatomic, strong) RSButton *shareButton;
 
 @end
 
@@ -21,26 +26,73 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"Artist";
     
+    [self setupUIElements];
+    [self setupAppearance];
+    
+}
+
+- (void)setupUIElements {
+    
+    //canvasView
     self.canvasView = [[CanvasView alloc] init];
     self.canvasView.backgroundColor = [UIColor whiteColor];
     self.canvasView.translatesAutoresizingMaskIntoConstraints = NO;
     
+    //openPaletteButton
+    self.openPaletteButton = [[RSButton alloc] init];
+    self.openPaletteButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.openPaletteButton setTitle:@"Open Palette" forState:UIControlStateNormal];
+    
+    //openTimerButton
+    self.openTimerButton = [[RSButton alloc] init];
+    self.openTimerButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.openTimerButton setTitle:@"Open Timer" forState:UIControlStateNormal];
+    
+    //drawButton
+    self.drawButton = [[RSButton alloc] init];
+    self.drawButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.drawButton setTitle:@"Draw" forState:UIControlStateNormal];
+    
+    //shareButton
+    self.shareButton = [[RSButton alloc] init];
+    self.shareButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.shareButton setTitle:@"Share" forState:UIControlStateNormal];
+    
+}
+
+- (void)setupAppearance {
+    
     [self.view addSubview:self.canvasView];
-    
-    
+    [self.view addSubview:self.openPaletteButton];
+    [self.view addSubview:self.openTimerButton];
+    [self.view addSubview:self.drawButton];
+    [self.view addSubview:self.shareButton];
     
     [NSLayoutConstraint activateConstraints:@[
+        //canvasView
         [self.canvasView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:40],
         [self.canvasView.heightAnchor constraintEqualToConstant:300],
         [self.canvasView.widthAnchor constraintEqualToConstant:300],
-        [self.canvasView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor]
+        [self.canvasView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        
+        //openPaletteButton
+        [self.openPaletteButton.topAnchor constraintEqualToAnchor:self.canvasView.bottomAnchor constant:50],
+        [self.openPaletteButton.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        
+        //openTimerButton
+        [self.openTimerButton.topAnchor constraintEqualToAnchor:self.openPaletteButton.bottomAnchor constant:20],
+        [self.openTimerButton.leadingAnchor constraintEqualToAnchor:self.openPaletteButton.leadingAnchor],
+        
+        //drawButton
+        [self.drawButton.topAnchor constraintEqualToAnchor:self.canvasView.bottomAnchor constant:50],
+        [self.drawButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-41],
+        
+        //shareButton
+        [self.shareButton.topAnchor constraintEqualToAnchor:self.drawButton.bottomAnchor constant:20],
+        [self.shareButton.trailingAnchor constraintEqualToAnchor:self.drawButton.trailingAnchor]
     ]];
 }
 
-- (void)setupUI {
-    
-    
-    
-}
+
 
 @end
